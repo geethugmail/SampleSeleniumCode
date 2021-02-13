09929bc8e551d0e9fa4c522aeacede592a91c8e1
geethu.bridget <geethu.bridget@BDC7-L-7QZ5QC2.dir.svc.accenture.com>
2021-02-13 20:00:33 +0530

    Modified code

--- src/test/java/sandbox/DockerTestChrome.java
+++ src/test/java/sandbox/DockerTestChrome.java
@@ -16,14 +16,21 @@ public class DockerTestChrome {
 		
 // 		DesiredCapabilities cap= new DesiredCapabilities();
 // 		cap.setBrowserName(BrowserType.CHROME);
-		ChromeOptions options = new ChromeOptions().setHeadless(true);	
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/app/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		System.setProperty("webdriver.chrome.args", "--disable-logging");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
 		WebDriver driver = new ChromeDriver(options);
 // 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
 		driver.get("https://buycloud.telstra.com");
 		driver.findElement(By.partialLinkText("Log")).click();
-		Thread.sleep(5000);
-	    Assert.assertTrue(driver.findElement(By.id("username")).isDisplayed());
+        Assert.assertEquals(driver.getTitle(),"Cloud Services Store");
 		Thread.sleep(5000);
 		driver.quit();
 	}
